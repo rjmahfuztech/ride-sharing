42d3890474dd7d02e849f1fd7a76f5cf1d16258d
rjmahfuztech <rjmahfuztech@gmail.com>
2025-09-07 19:59:27 +0600

    two method added in Rider class and Driver class created

--- users.py
+++ users.py
@@ -30,3 +30,22 @@ class Rider(User):
 
     def update_location(self, current_location):
         self.current_location = current_location
+
+    def request_ride(self, ride_sharing, destination):
+        ...
+
+    def current_ride(self):
+        print(self.current_ride)
+
+
+class Driver(User):
+    def __init__(self, name, email, nid, current_location):
+        self.current_location = current_location
+        self.wallet = 0
+        super().__init__(name, email, nid)
+
+    def display_profile(self, ride):
+        print(f"Driver Name: {self.name}")
+
+    def accept_ride(self, ride):
+        ...
